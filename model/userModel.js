15b9eaeea332641ce9632cdd7d314131eae6bcd7
harshid-hadiya <ahirharshidj@gmail.com>
2025-12-29 16:26:08 +0530

    change in the model

--- model/userModel.js
+++ model/userModel.js
@@ -1,12 +1,18 @@
-const mongoose=require("mongoose")
-const userModel=mongoose.Schema({
-  name:{type:String,required:true,trim:true},
-  username:{type:String,required:true,trim:true,unique:true},
-  isRider:{type:Boolean,default:false},
-  rideType:{type:String,enum:["BIKE","CAR","AUTO"],default:""},
-  email:{type:String,required:true,trim:true},
-  password:{type:String,required:true},
-  Mobile:{type:Number,required:[true,"without mobile number how we contact you ?"]} 
-},{timestamps:true})
-const User=mongoose.model("User",userModel);
-module.exports=User;
+const mongoose = require("mongoose");
+const userModel = mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    username: { type: String, required: true, trim: true, unique: true },
+    isRider: { type: Boolean, default: false },
+    rideType: { type: String, enum: ["BIKE", "CAR", "AUTO"], default: "AUTO" },
+    email: { type: String, required: true, trim: true },
+    password: { type: String, required: true },
+    Mobile: {
+      type: Number,
+      required: [true, "without mobile number how we contact you ?"],
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userModel);
+module.exports = User;
